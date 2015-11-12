126631b847dc20e9f3ed64f31fb6f01b63a56184
Bui Vo Quoc Bao <bvqbao@gmail.com>
2015-11-12 22:20:25 +0700

    Refactor Language class

--- app/Core/Language.php
+++ app/Core/Language.php
@@ -3,14 +3,13 @@
  * Language - simple language handler.
  *
  * @author Bartek Kuśmierczuk - contact@qsma.pl - http://qsma.pl
+ * @author Bui Vo Quoc Bao
  * @version 2.2
  * @date November 18, 2014
  * @date updated November 12, 2015
  */
 
 namespace Core;
-
-use Core\Error;
 
 /**
  * Language class to load the requested language file.
@@ -34,25 +33,18 @@ class Language
     /**
      * Load language function.
      *
-     * @param string $name
-     * @param string $code
+     * @param string $name name of language file
+     * @param string $code optional, language code
      */
     public function load($name, $code = null)
     {
-        /** if there is no specified language, use the default one */
-        $code = $code ?: self::getLocale() ;
+        $code = $code ?: self::getLocale();
 
-        /** lang file */
-        $file = SMVC."app/language/$code/$name.php";
-
-        /** check if is readable */
-        if (is_readable($file)) {
-            /** require file */
-            $this->array = include($file);
-        } else {
-            /** display error */
-            echo Error::display("Could not load language file '$code/$name.php'");
-            die;
+        try {
+            $this->array = self::tryToLoad($name, $code);
+        } catch (\Exception $e) {
+            Logger::newMessage($e);
+            Logger::customErrorMsg();
         }
     }
 
@@ -70,33 +62,26 @@ class Language
         } else {
             return $value;
         }
-    }
+    }   
 
     /**
      * Get lang for views.
      *
      * @param  string $value this is "word" value from language file
-     * @param  string $name  name of file with language
+     * @param  string $name  name of language file
      * @param  string $code  optional, language code
      *
      * @return string
      */
     public static function show($value, $name, $code = null)
     {
-        /** if there is no specified language, use the default one */
-        $code = $code ?: self::getLocale() ;
+        $code = $code ?: self::getLocale();
 
-        /** lang file */
-        $file = SMVC."app/language/$code/$name.php";
-
-        /** check if is readable */
-        if (is_readable($file)) {
-            /** require file */
-            $array = include($file);
-        } else {
-            /** display error */
-            echo Error::display("Could not load language file '$code/$name.php'");
-            die;
+        try {
+            $array = self::tryToLoad($name, $code);
+        } catch (\Exception $e) {
+            Logger::newMessage($e);
+            Logger::customErrorMsg();
         }
 
         if (!empty($array[$value])) {
@@ -123,8 +108,23 @@ class Language
      */
     public static function setLocale($locale)
     {
+        self::assertValidLocale($locale);
         self::$locale = $locale;
     }
+
+    /**
+     * Asserts that the locale is valid, throws an Exception if not.
+     *
+     * @param string $locale Locale to test
+     *
+     * @throws \InvalidArgumentException If the locale contains invalid characters
+     */
+    private static function assertValidLocale($locale)
+    {
+        if (1 !== preg_match('/^[a-z0-9@_\\.\\-]*$/i', $locale)) {
+            throw new \InvalidArgumentException(sprintf('Invalid "%s" locale.', $locale));
+        }
+    }    
 
     /**
      * Load a language file.
@@ -135,20 +135,34 @@ class Language
      */
     public static function silentlyLoad($name, $code = null)
     {
-        /** if there is no specified language, use the default one */
-        $code = $code ?: self::getLocale() ;
+        $code = $code ?: self::getLocale();
 
-        $content = [];
-
-        /** lang file */
-        $file = SMVC."app/language/$code/$name.php";
-
-        /** check if is readable */
-        if (is_readable($file)) {
-            /** require file */
-            $content = include($file);
+        try {
+            $content = self::tryToLoad($name, $code);
+        } catch (\Exception $e) {
+            Logger::newMessage($e);
+            /** return an empty array if something went wrong */
+            $content = [];
         }
 
         return $content;
     } 
+
+    /**
+     * Try to load a language file using file name and language code.
+     * 
+     * @param  string $name name of language file
+     * @param  string $code optional, language code
+     * @return array
+     * @throws \Exception if the language file is not readable
+     */
+    private static function tryToLoad($name, $code)
+    {
+        $file = SMVC."app/language/$code/$name.php";
+        if (is_readable($file)) {
+            return include($file);
+        } else {
+            throw new \Exception(sprintf('File "%s" is not readable.', $file));
+        }
+    }
 }
