b4a2f508d83be8065ef122e6d1e11d6187a10889
Bui Vo Quoc Bao <bvqbao@gmail.com>
2015-12-02 17:35:41 +0700

    Remove all trailing slashes

---
+++ app/Http/Middleware/RemoveTrailingSlashes.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Http\Middleware;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class RemoveTrailingSlashes {
+
+	/**
+	 * Remove all trailing slashes from the request uri.
+	 * 
+	 * @param  Request  $request
+	 * @param  Response $response
+	 * @param  callable $next
+	 * @return Response
+	 */
+	public function __invoke(Request $request, Response $response, callable $next) {
+	    $uri = $request->getUri();
+	    $path = $uri->getPath();
+	    if ($path != '/' && substr($path, -1) == '/') {
+	        $uri = $uri->withPath(trim($path, '/'));
+	        $request = $request->withUri($uri);
+	    }
+
+	    return $next($request, $response);
+	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,23 @@
 <?php
-/**
- * We can use a Closure or a controller method (class:method) to handle a route.
- */
 
 /*
 |--------------------------------------------------------------------------
-| Welcome Pages
+| Application Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| We can use a Closure or a controller method (class:method) to handle a route.
+|
 */
 
 $app->get('/', 'Http\Controllers\Welcome:welcome');
 $app->get('/subpage', 'Http\Controllers\Welcome:subpage');
+
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+*/
+
+$app->add(new \Http\Middleware\RemoveTrailingSlashes());
